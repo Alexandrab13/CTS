839a2ab8fa7781091e60be04b838c57c5d06a876
Boghirnea Alexandra <115037492+Alexandrab13@users.noreply.github.com>
2023-05-11 19:09:21 +0300

    Add files via upload

---
+++ Template/Template/src/Program.java
@@ -0,0 +1,11 @@
+public class Program
+{
+    public static void main(String[] args)
+    {
+        Prajitura prajitura1 = new PrajituraCiocolata();
+        prajitura1.facemPrajitura();
+
+        Prajitura prajitura2 = new PrajituraFructe();
+        prajitura2.facemPrajitura();
+    }
+}
